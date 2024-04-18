06e79226214c54db0a61e928bef0515d3be828cd
Nguyen Tran <nguyentt1@vnpay.vn>
2024-04-18 17:02:54 +0700

    updated s3 media helper test

--- src/helper/media/media.go
+++ src/helper/media/media.go
@@ -7,7 +7,6 @@ import (
 	"fmt"
 	"io"
 	"net/url"
-	"os"
 	"path/filepath"
 	"time"
 
@@ -15,19 +14,19 @@ import (
 	custerror "github.com/CE-Thesis-2023/backend/src/internal/error"
 	"github.com/CE-Thesis-2023/backend/src/internal/logger"
 	"github.com/CE-Thesis-2023/backend/src/models/db"
-	"github.com/aws/aws-sdk-go-v2/config"
-	"github.com/aws/aws-sdk-go-v2/credentials"
-	"github.com/aws/aws-sdk-go-v2/service/s3"
-	smithyendpoints "github.com/aws/smithy-go/endpoints"
-	"github.com/aws/smithy-go/logging"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/credentials"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/s3"
+
 	"go.uber.org/zap"
 )
 
 type MediaHelper struct {
-	configs       *configs.MediaMtxConfigs
-	s3Configs     *configs.S3Storage
-	s3Client      *s3.Client
-	presignClient *s3.PresignClient
+	configs   *configs.MediaMtxConfigs
+	s3Configs *configs.S3Storage
+	s3Client  *s3.S3
 }
 
 func NewMediaHelper(configs *configs.MediaMtxConfigs, s3 *configs.S3Storage) *MediaHelper {
@@ -42,43 +41,19 @@ func NewMediaHelper(configs *configs.MediaMtxConfigs, s3 *configs.S3Storage) *Me
 	return h
 }
 
-type s3EndpointResolver struct {
-	Endpoint string
-}
-
-func (r *s3EndpointResolver) ResolveEndpoint(ctx context.Context, params s3.EndpointParameters) (smithyendpoints.Endpoint, error) {
-	u, err := url.Parse(r.Endpoint)
-	if err != nil {
-		return smithyendpoints.Endpoint{}, err
-	}
-	return smithyendpoints.Endpoint{
-		URI: *u,
-	}, nil
-}
-
 func (m *MediaHelper) initS3Client() error {
-	awsConfigs, err := config.LoadDefaultConfig(
-		context.Background(),
-		config.WithRegion(m.s3Configs.Region),
-		config.WithLogger(logging.NewStandardLogger(os.Stdout)),
-		config.WithCredentialsProvider(credentials.NewStaticCredentialsProvider(
+	awsConfigs := aws.NewConfig().
+		WithEndpoint(m.s3Configs.Endpoint).
+		WithRegion(m.s3Configs.Region).
+		WithCredentials(credentials.NewStaticCredentials(
 			m.s3Configs.AccessKeyID,
 			m.s3Configs.Secret,
-			""),
-		),
-	)
+			""))
+	sess, err := session.NewSession(awsConfigs)
 	if err != nil {
-		logger.SError("failed to load S3 config",
-			zap.Error(err))
 		return err
 	}
-	client := s3.NewFromConfig(awsConfigs, func(o *s3.Options) {
-		o.EndpointResolverV2 = &s3EndpointResolver{
-			Endpoint: m.s3Configs.Endpoint,
-		}
-	})
-	m.s3Client = client
-	m.presignClient = s3.NewPresignClient(client)
+	m.s3Client = s3.New(sess)
 	return nil
 }
 
@@ -145,11 +120,12 @@ func (m *MediaHelper) UploadImage(ctx context.Context, req *UploadImageRequest) 
 		return custerror.FormatInvalidArgument("failed to decode image: %v", err)
 	}
 	reader := bytes.NewReader(decodedImg)
-	_, err = m.s3Client.PutObject(ctx, &s3.PutObjectInput{
-		Bucket: &m.s3Configs.Bucket,
-		Key:    m.getImageBasePath(req.Path),
-		Body:   reader,
-	})
+	_, err = m.s3Client.PutObjectWithContext(
+		ctx, &s3.PutObjectInput{
+			Bucket: &m.s3Configs.Bucket,
+			Key:    m.getImageBasePath(req.Path),
+			Body:   reader,
+		})
 	if err != nil {
 		return custerror.FormatInternalError("failed to upload image: %v", err)
 	}
@@ -161,7 +137,7 @@ type GetImageResponse struct {
 }
 
 func (m *MediaHelper) GetImage(ctx context.Context, path string) (*GetImageResponse, error) {
-	objRef, err := m.s3Client.GetObject(ctx, &s3.GetObjectInput{
+	objRef, err := m.s3Client.GetObjectWithContext(ctx, &s3.GetObjectInput{
 		Bucket: &m.s3Configs.Bucket,
 		Key:    m.getImageBasePath(path),
 	})
@@ -179,19 +155,21 @@ func (m *MediaHelper) GetImage(ctx context.Context, path string) (*GetImageRespo
 }
 
 func (m *MediaHelper) GetPresignedUrl(ctx context.Context, path string) (string, error) {
-	presignedReq, err := m.presignClient.PresignGetObject(ctx, &s3.GetObjectInput{
+	objRef, _ := m.s3Client.GetObjectRequest(&s3.GetObjectInput{
 		Bucket: &m.s3Configs.Bucket,
 		Key:    m.getImageBasePath(path),
-	},
-		s3.WithPresignExpires(15*time.Minute),
-	)
+	})
+	presignedUrl, err := objRef.Presign(15 * time.Minute)
 	if err != nil {
-		return "", custerror.FormatInternalError("failed to get presigned URL: %v", err)
+		return "", custerror.FormatInternalError("failed to presign URL: %v", err)
 	}
-	return presignedReq.URL, nil
+	return presignedUrl, nil
 }
 
 func (m *MediaHelper) getImageBasePath(id string) *string {
-	p := filepath.Join(m.s3Configs.PathPrefix, "people", id)
+	p := filepath.Join(
+		m.s3Configs.PathPrefix,
+		"people",
+		id)
 	return &p
 }
--- src/helper/media/tests/media_test.go
+++ src/helper/media/tests/media_test.go
@@ -1,29 +1,32 @@
-package media
+package media_tests
 
 import (
 	"context"
+	"encoding/base64"
 	"fmt"
+	"log"
 	"os"
 	"testing"
 	"time"
 
+	"github.com/CE-Thesis-2023/backend/src/helper/media"
 	"github.com/CE-Thesis-2023/backend/src/internal/configs"
 	custdb "github.com/CE-Thesis-2023/backend/src/internal/db"
 	"github.com/CE-Thesis-2023/backend/src/internal/logger"
 	"github.com/CE-Thesis-2023/backend/src/models/db"
 )
 
-func initBiz() *MediaHelper {
+func initBiz() *media.MediaHelper {
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
 
-	os.Setenv("CONFIG_FILE_PATH", "../../../configs.json")
+	os.Setenv("CONFIG_FILE_PATH", "../../../../configs.json")
 	configs.Init(ctx)
 	logger.Init(ctx,
 		logger.WithGlobalConfigs(&configs.Get().Logger))
 
 	custdb.Init(ctx, configs.Get())
-	mediaHelper := NewMediaHelper(
+	mediaHelper := media.NewMediaHelper(
 		&configs.Get().MediaEngine,
 		&configs.Get().S3)
 	return mediaHelper
@@ -66,6 +69,44 @@ func TestMediaHelper_WebRTCViewUrl(t *testing.T) {
 	mediaHelper := initBiz()
 
 	url := mediaHelper.BuildWebRTCViewStream("test")
+
+	if url == "" {
+		t.Error("url is empty")
+	}
+
+	fmt.Println(url)
+}
+
+func prepareTestImg(path string) string {
+	content, err := os.ReadFile("./testdata/" + path)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return base64.StdEncoding.EncodeToString(content)
+}
+
+func TestMediaHelper_UploadImage(t *testing.T) {
+	mediaHelper := initBiz()
+	encodedImg := prepareTestImg("bona4.jpg")
+
+	err := mediaHelper.UploadImage(context.Background(), &media.UploadImageRequest{
+		Base64Image: encodedImg,
+		Path:        "bona4.jpg",
+	})
+	if err != nil {
+		t.Error(err)
+	}
+}
+
+func TestMediaHelper_GetPresignedUrl(t *testing.T) {
+	mediaHelper := initBiz()
+
+	url, err := mediaHelper.GetPresignedUrl(
+		context.Background(),
+		"bona4.jpg")
+	if err != nil {
+		t.Error(err)
+	}
 
 	if url == "" {
 		t.Error("url is empty")
